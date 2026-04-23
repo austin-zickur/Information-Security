f53677cc9984df80ea18d0f701869c44f0de3411
austin-zickur <zickuraustin.work@gmail.com>
2026-04-22 23:57:19 -0500

    Set Content Security Policy (CSP) directives to "'self'"

--- Stock Price Checker/server.js
+++ Stock Price Checker/server.js
@@ -10,6 +10,19 @@ const runner            = require('./test-runner');
 
 const app = express();
 
+/*Security Measures
+Author: Austin Zickur
+Boilerplate: freeCodeCamp InfoSec*/
+const helmet = require('helmet');
+app.use(helmet.contentSecurityPolicy({
+  directives:{
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'"],
+    styleSrc: ["'self'"]
+  }
+}));
+/*END*/
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
